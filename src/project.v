9d96f3cffbd410bc25bad80a8825d541a71ef57f
Yorochomin <t322072@m.chukyo-u.ac.jp>
2024-11-28 16:30:10 +0900

    Update project.v

--- src/project.v
+++ src/project.v
@@ -14,8 +14,10 @@ module tt_um_example (
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n,     // reset_n - low to reset
-    input wire [2:0] A, B,
-    output wire [0:0] Q, COUT
+    input wire [1:0] A,
+    input wire [1:0] B,
+    output wire [0:0] Q,
+    output wire [0:0] COUT
 );
 
   // All output pins must be assigned. If not used, assign to 0.
